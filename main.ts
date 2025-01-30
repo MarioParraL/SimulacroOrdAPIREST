418c929126a3042d75ddda4d6319ae7400065c89
MarioParraL <mariovidovick@gmail.com>
2025-01-30 18:14:30 +0100

    a

--- main.ts
+++ main.ts
@@ -3,14 +3,15 @@ import { MongoClient } from "mongodb";
 const MONGO_URL = Deno.env.get("MONGO_URL");
 if (!MONGO_URL) {
   console.error("Please provide a MONGO_URL");
+  Deno.exit(1);
 }
 
 const client = new MongoClient(MONGO_URL);
 await client.connect();
 console.info("Connected to MongoDB");
 
-const mongodb = client.db("SimulacroApiRest1DB");
-const ContactsCollection = mongodb.collection("contacts");
+const db = client.db("SimulacroApiRest1DB");
+const ContactsCollection = db.collection("contacts");
 
 const handler = async (req: Request): Promise<Response> => {
   const method = req.method;
